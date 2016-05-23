1bc37605edcce6628a892dfc5b41a700c3421e3d
Simon Bennie <simon.bennie@bristol.ac.uk>
2016-05-23 11:10:32 +0100

    try and repush old version

--- Pymol/__init__.py
+++ Pymol/__init__.py
@@ -7,23 +7,23 @@ import subprocess
 
 
 def __init__(self):
-    self.menuBar.addmenuitem('Plugin', 'command', 'enzlig', label='enlighten', command=lambda: mainDialog())
+    self.menuBar.addmenuitem('Plugin', 'command', 'enlighten', label='enlighten', command=lambda : mainDialog())
 
 
 # Diaglogue lables
-class enzlig(Frame):
+class enlighten(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         # Parent means the frame that is being used
         self.parent = parent  # This what calls the ezlig(root)
         self.initUI()
-        # Pass the junk variables sot hat dialouge is clean
+
         self.selection = ""
         self.pdb = ""
 
     def initUI(self):
         # There are four frames in the window containing varios buttons/dropdowns etc
-        self.parent.title("EnzLig Tools")
+        self.parent.title("Enlighten Tools")
         frame4 = Frame(self.parent)
         frame4.grid(row=0, column=0, sticky="nsew")
         self.fv = IntVar()
@@ -48,33 +48,39 @@ class enzlig(Frame):
         self.entry1.grid(row=0, column=1, columnspan=4, sticky=W + E)
         self.browserButton = Button(frame1, text="Browser", command=self.onOpenF)
         self.browserButton.grid(row=0, column=5, sticky="e")
-        lbl2 = Label(frame1, text="Enzlig Folder", width=12)
+        lbl2 = Label(frame1, text="enlighten Folder", width=12)
         lbl2.grid(row=1, column=0)
-        self.entry2 = Entry(frame1)
-        self.entry2.grid(row=1, column=1, columnspan=4, sticky=W + E)
-        enzligButton = Button(frame1, text="Browser", command=self.onOpen)
-        enzligButton.grid(row=1, column=5, sticky="e")
+        self.enlightenpath = Entry(frame1)
+        self.enlightenpath.insert(END, '/Users/simonbennie/enlighten') #fixme
+        self.enlightenpath.grid(row=1, column=1, columnspan=4, sticky=W + E)
+        enlightenButton = Button(frame1, text="Browser", command=self.onOpen)
+        enlightenButton.grid(row=1, column=5, sticky="e")
         lbl3 = Label(frame1, text="AMBER Folder", width=12)
         lbl3.grid(row=2, column=0)
-        self.entry3 = Entry(frame1)
-        self.entry3.grid(row=2, column=1, columnspan=4, sticky=W + E)
+        self.amberpath = Entry(frame1)
+        self.amberpath.grid(row=2, column=1, columnspan=4, sticky=W + E)
         amberButton = Button(frame1, text="Browser", command=self.onOpenA)
         amberButton.grid(row=2, column=5, sticky="e")
         # This will auto find the amber installation from the environment variables
-        self.entry3.insert(0, os.environ.get('AMBERHOME', 'Please specify AMBER home directory'))
+        self.amberpath.insert(0, os.environ.get('AMBERHOME', 'Please specify AMBER home directory'))
+        # self.amberpath.insert(END, '/Users/simonbennie/bin/amber14') # Fixme
+
         lbl4 = Label(frame1, text="Output Folder", width=12)
         lbl4.grid(row=3, column=0)
-        self.entry4 = Entry(frame1)
-        self.entry4.grid(row=3, column=1, columnspan=4, sticky=W + E)
+        self.workingpath = Entry(frame1)
+        self.workingpath.insert(END, '/Users/simonbennie') # Fixme
+        self.workingpath.grid(row=3, column=1, columnspan=4, sticky=W + E)
         outputButton = Button(frame1, text="Browser", command=self.onOpenO)
         outputButton.grid(row=3, column=5, sticky="e")
         lbl5 = Label(frame1, text="Ligand Name", width=12)
         lbl5.grid(row=4, column=0)
-        self.entry5 = Entry(frame1)
-        self.entry5.grid(row=4, column=1)
+        self.ligandname = Entry(frame1)
+        self.ligandname.insert(END, '0RN') # Fixme
+        self.ligandname.grid(row=4, column=1)
         lbl6 = Label(frame1, text="Ligand Charge", width=12)
         lbl6.grid(row=4, column=2)
         self.entry6 = Entry(frame1)
+        self.entry6.insert(END, '-1')
         self.entry6.grid(row=4, column=3)
 
         # This is where frame two starts
@@ -94,23 +100,12 @@ class enzlig(Frame):
         self.lb1.config(state=DISABLED)
 
         # Time steps for use in dynamm
-        lbl7 = Label(frame1, text="Time steps", width=12)
+        lbl7 = Label(frame1, text="Time steps (ps)", width=12)
         lbl7.grid(row=5, column=2)
         self.entry7 = Entry(frame1)
+        self.entry7.insert(END, '100')
         self.entry7.grid(row=5, column=3)
-
-
-#        group = Pmw.Group(page,tag_text='Dynamics variables')
-#        group.pack(fill = 'both', expand = 1, padx = 4, pady = 5)
-#        group.grid(column=5, row=3)
-#        self.entry7 = Pmw.EntryField(group.interior(),labelpos='w',
-#                                   label_text = 'Protein Dielectric:',
-#                                   value = "2000",# str(APBSTools2.defaults['interior_dielectric']),
-#                                   validate = {'validator' : 'real',
-#                                               'min':0,}
-#                                   )
-
-        print(self.entry7)
+        self.nstlim = int(self.entry7.get())/500  # Convert to time steps
 
         # self.vsb.config(state=DISABLED)
         frame3 = Frame(self.parent)
@@ -121,9 +116,9 @@ class enzlig(Frame):
         self.structButton = Button(frame3, text="RUN STRUCT", command=self.runstruct)
         self.structButton.grid(row=0, column=1, sticky="e")
         self.structButton.config(state=DISABLED)
-        self.structButton = Button(frame3, text="RUN DYNAM", command=self.rundynam)
-        self.structButton.grid(row=0, column=2, sticky="e")
-        self.structButton.config(state=DISABLED)
+        self.dynamButton = Button(frame3, text="RUN DYNAM", command=self.rundynam)
+        self.dynamButton.grid(row=0, column=2, sticky="e")
+        self.dynamButton.config(state=DISABLED)
 
     # This next section defines a series of dialogues that are opened according the the actions from above
     def onOpenF(self):
@@ -131,23 +126,23 @@ class enzlig(Frame):
         self.entry1.delete(0, 'end')
         self.entry1.insert(0, pdb)
         path = os.path.split(pdb)
-        self.entry4.delete(0, 'end')
-        self.entry4.insert(0, path[0])
+        self.workingpath.delete(0, 'end')
+        self.workingpath.insert(0, path[0])
 
     def onOpenA(self):
         amber = tkFileDialog.askdirectory()
-        self.entry3.delete(0, 'end')
-        self.entry3.insert(0, amber)
+        self.amberpath.delete(0, 'end')
+        self.amberpath.insert(0, amber)
 
     def onOpenO(self):
         out = tkFileDialog.askdirectory()
-        self.entry4.delete(0, 'end')
-        self.entry4.insert(0, out)
+        self.workingpath.delete(0, 'end')
+        self.workingpath.insert(0, out)
 
     def onOpen(self):
         fold = tkFileDialog.askdirectory()
-        self.entry2.delete(0, 'end')
-        self.entry2.insert(0, fold)
+        self.enlightenpath.delete(0, 'end')
+        self.enlightenpath.insert(0, fold)
 
     def OnVsb(self, *args):
         self.lb1.yview(*args) # Moves the scroll bar
@@ -218,12 +213,12 @@ class enzlig(Frame):
                 # print self.sv.get()
 
     def runprep(self):
-        if (self.fv.get() == 1):
-            os.chdir(self.entry4.get())
-            os.environ["AMBERHOME"] = self.entry3.get()
+        if self.fv.get() == 1:
+            os.chdir(self.workingpath.get())
+            os.environ["AMBERHOME"] = self.amberpath.get()
             # Saves the command for use
-            command = self.entry2.get() + "/prep.sh " + os.path.basename(
-                self.entry1.get()) + " " + self.entry5.get() + " " + self.entry6.get()
+            command = self.enlightenpath.get() + "/prep.sh " + os.path.basename(
+                self.entry1.get()) + " " + self.ligandname.get() + " " + self.entry6.get()
             #Executes the comman passed above
             p = subprocess.Popen([command], shell=True, stderr=subprocess.PIPE)
             # This initates a wait for the output to complete before the next stage is run
@@ -234,7 +229,7 @@ class enzlig(Frame):
                 if out != '':
                     sys.stdout.write(out)
                     sys.stdout.flush()
-            print "Job Finished"
+            print("Job Finished")
             path = os.path.split(self.entry1.get())
             temp = path[1].split('.')
             # First loads the topology file
@@ -243,40 +238,45 @@ class enzlig(Frame):
             pymol.cmd.load("./" + temp[0] + "/" + temp[0] + ".sp20.pdb", temp[0] + ".sp20")
             self.pdb = os.path.basename(self.entry1.get())
         # The difference between this and obe is the entry selection
-        if (self.sv.get() == 1):
-            os.chdir(self.entry4.get())
-            cmd.save(self.entry4.get() + "/" + self.selection + ".pdb", self.selection)
-            os.environ["AMBERHOME"] = self.entry3.get()
+        if self.sv.get() == 1:
+            os.chdir(self.workingpath.get())
+            print(os.chdir(self.workingpath.get()))
+            cmd.save(self.workingpath.get() + "/" + self.selection + ".pdb", self.selection)
+            os.environ["AMBERHOME"] = self.amberpath.get()
             os.environ["PATH"] = os.environ["PATH"] + ":" + os.environ["AMBERHOME"] + "/bin"
-            command = self.entry2.get() + "/prep.sh " + self.selection + ".pdb" + " " + self.entry5.get() + " " + self.entry6.get()
+            command = self.enlightenpath.get() + "/prep.sh " + self.selection + ".pdb" + " " + self.ligandname.get() +\
+                      " " + self.entry6.get()
             p = subprocess.Popen([command], shell=True, stderr=subprocess.PIPE)
             while True:
                 out = p.stderr.read(1)
-                if out == '' and p.poll() != None:
-                    break
-                if out != '':
-                    sys.stdout.write(out)
-                    sys.stdout.flush()
+                # if out == '' and p.poll() != None:
+                #     break
+                # if out != '':
+                sys.stdout.write(out)
+                sys.stdout.flush()
             print "Job Finished"
             temp = self.selection
+            print(temp)
             pymol.cmd.load("./" + temp + "/" + temp + ".sp20.top", temp + ".sp20")
-            pymol.cmd.load("./" + temp + "/" + temp + ".sp20.pdb", temp + ".sp20")
+            pymol.cmd.load("./" + temp +"/" + temp +  ".sp20.pdb", temp + ".sp20") # fixme
             self.pdb = self.selection + ".pdb"
         self.structButton.config(state="normal")
-        os.environ["ENZLIG"] = self.entry2.get()
+        os.environ["enlighten"] = self.enlightenpath.get()
 
     def runstruct(self):
-        command = self.entry2.get() + "/struct.sh " + self.pdb + " " + self.entry5.get() + " " + self.entry6.get()
+        command = self.enlightenpath.get() + "/struct/struct.sh " + self.pdb + " " + self.ligandname.get() +\
+                  " " + self.entry6.get()
         p = subprocess.Popen([command], shell=True, stderr=subprocess.PIPE)
         while True:
             out = p.stderr.read(1)
-            if out == '' and p.poll() != None:
-                break
-            if out != '':
-                sys.stdout.write(out)
-                sys.stdout.flush()
+            #if out == '' and p.poll() != None:
+            #    break
+            #if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
         temp = self.pdb[:-4]
-        pymol.cmd.load("./" + temp + "/struct/min_sa_" + temp + ".sp20.rst", temp + ".sp20")
+        pymol.cmd.load("./" + temp +"/"+ temp + ".sp20.rst", temp + ".sp20")
+        self.dynamButton.config(state="normal")
         print "Job Finished"
 
     # Run a dynamics calculation on the given structure
@@ -288,21 +288,21 @@ class enzlig(Frame):
         top=temp+".sp20.top"
         rst=temp+".sp20.rst"
 
-        dynam(self.entry3,top,rst,nstlim)
+        dynam(self.amberpath,top,rst,self.nstlim,self.ligandname)
         while True:
             out = p.stderr.read(1)
-            if out == '' and p.poll() != None:
-                break
-            if out != '':
-                sys.stdout.write(out)
-                sys.stdout.flush()
+           # if out == '' and p.poll() != None:
+           #     break
+           # if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
         temp = self.pdb[:-4]
-        pymol.cmd.load("./" + temp + "/struct/min_sa_" + temp + ".sp20.rst", temp + ".sp20")
+        pymol.cmd.load("./" + temp + "/struct/min_sa_" + temp + ".sp20.trj", temp + ".trj")
         print "Job Finished"
 
 def mainDialog():
     # Tk is a tinker python gui library, This is the default toolkit used in pymol
     root = Tk()
     root.resizable(0, 0)  # currently not resizable due to lack of reflow
-    enzlig(root)  # This is where the nezlig class is called, passes the parent frame
+    enlighten(root)  # This is where the nezlig class is called, passes the parent frame
     root.mainloop()

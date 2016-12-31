53c5d7f20128385c160b44ed73ea4519c8345a9d
Dr Simon Bennie <sb13343@bristolac.uk>
2016-12-31 06:17:47 +0000

    visual redesign of plugin, backend doesnt work yet

--- Pymol/__init__.py
+++ Pymol/__init__.py
@@ -26,115 +26,131 @@ class enlighten(Frame):
 
         # There are four frames in the window containing various buttons/dropdowns etc
         self.parent.title("Enlighten Tools")
-        frame4 = Frame(self.parent)
-        frame4.grid(row=0, column=0, sticky="nsew")
+        frame1 = Frame(self.parent)
+        frame1.grid(row=0, column=0, sticky="nsew")
         self.fv = IntVar()
         self.fv.set(1)
-        self.file = Checkbutton(frame4, text="Choose from file", command=self.selectoption1, variable=self.fv,
+        self.file = Checkbutton(frame1, text="Choose from file", command=self.selectoption1, variable=self.fv,
                                 offvalue=0, onvalue=1)
         self.file.grid(row=0, column=0)
         self.file.select()
         self.sv = IntVar()
         self.sv.set(0)
-        self.sel = Checkbutton(frame4, text="Choose from PyMOL object", command=self.selectoption2, variable=self.sv,
+        self.sel = Checkbutton(frame1, text="Choose from PyMOL object", command=self.selectoption2, variable=self.sv,
                                offvalue=0, onvalue=1)
         self.sel.grid(row=0, column=1)
         self.sel.deselect()
 
         # Now move onto frame one
-        frame1 = Frame(self.parent)
-        frame1.grid(row=1, column=0)
-        lbl1 = Label(frame1, text="PDB file", width=12)
+        frame2 = Frame(self.parent)
+        frame2.grid(row=1, column=0, sticky=W, columnspan=6)
+        lbl1 = Label(frame2, text="PDB file", width=12)
         lbl1.grid(row=0, column=0)
-        self.entry1 = Entry(frame1)
-        self.entry1.grid(row=0, column=1, columnspan=4, sticky=W + E)
-        self.browserButton = Button(frame1, text="Browser", command=self.onOpenF)
-        self.browserButton.grid(row=0, column=5, sticky="e")
+        self.entry1 = Entry(frame2)
+        self.entry1.grid(row=0, column=1, sticky=W+E, columnspan=6)
+        self.browserButton = Button(frame2, text="Browser", command=self.onOpenF)
+        self.browserButton.grid(row=0, column=6, sticky="e")
 
-        lbl2 = Label(frame1, text="Enlighten folder", width=12)
+        lbl2 = Label(frame2, text="Enlighten folder", width=12)
         lbl2.grid(row=1, column=0)
-        self.enlightenpath = Entry(frame1)
+        self.enlightenpath = Entry(frame2)
         #self.enlightenpath.insert(END, '/Users/simonbennie/enlighten') #fixme
         self.enlightenpath.insert(0, os.environ.get('ENLIGHTEN', 'Please specify ENLIGHTEN home directory'))
-        self.enlightenpath.grid(row=1, column=1, columnspan=4, sticky=W + E)
-        enlightenButton = Button(frame1, text="Browser", command=self.onOpen)
-        enlightenButton.grid(row=1, column=5, sticky="e")
-        lbl3 = Label(frame1, text="AMBER folder", width=12)
+        self.enlightenpath.grid(row=1, column=1, columnspan=6, sticky=W+E)
+        enlightenButton = Button(frame2, text="Browser", command=self.onOpen)
+        enlightenButton.grid(row=1, column=6, sticky=E)
+
+        lbl3 = Label(frame2, text="AMBER folder", width=12)
         lbl3.grid(row=2, column=0)
-        self.amberpath = Entry(frame1)
-        self.amberpath.grid(row=2, column=1, columnspan=4, sticky=W + E)
-        amberButton = Button(frame1, text="Browser", command=self.onOpenA)
-        amberButton.grid(row=2, column=5, sticky="e")
+        self.amberpath = Entry(frame2)
+        self.amberpath.grid(row=2, column=1, columnspan=6, sticky=W+E)
+        amberButton = Button(frame2, text="Browser", command=self.onOpenA)
+        amberButton.grid(row=2, column=6, sticky="e")
         # This will auto find the amber installation from the environment variables
         self.amberpath.insert(0, os.environ.get('AMBERHOME', 'Please specify AMBER home directory'))
 
-        lbl4 = Label(frame1, text="Output folder", width=12)
+        lbl4 = Label(frame2, text="Output folder", width=12)
         lbl4.grid(row=3, column=0)
-        self.workingpath = Entry(frame1)
+        self.workingpath = Entry(frame2)
         self.workingdir=os.getcwd() #pymol.externing.pwd()
         self.workingpath.insert(END,os.getcwd())  # Fixme
-        self.workingpath.grid(row=3, column=1, columnspan=4, sticky=W + E)
-        outputButton = Button(frame1, text="Browser", command=self.onOpenO)
-        outputButton.grid(row=3, column=5, sticky="e")
+        self.workingpath.grid(row=3, column=1, columnspan=6, sticky=W+E)
+        outputButton = Button(frame2, text="Browser", command=self.onOpenO)
+        outputButton.grid(row=3, column=6, sticky="e")
 
-        lbl5 = Label(frame1, text="Ligand Name", width=12)
+        lbl5 = Label(frame2, text="Ligand Name", width=12)
         lbl5.grid(row=4, column=0)
-        self.ligandname = Entry(frame1)
+        self.ligandname = Entry(frame2, width=10)
         self.ligandname.insert(END, 'Ligand name') # Fixme
         self.ligandname.grid(row=4, column=1)
 
-        lbl6 = Label(frame1, text="Ligand Charge", width=12)
+        lbl6 = Label(frame2, text="Ligand Charge", width=12)
         lbl6.grid(row=4, column=2)
-        self.ligandcharge = Entry(frame1)
+        self.ligandcharge = Entry(frame2,width=5)
         self.ligandcharge.insert(END, '-1')
         self.ligandcharge.grid(row=4, column=3)
 
         # Time steps for use in dynam
-        lbl7 = Label(frame1, text="Time (ps)", width=12)
-        lbl7.grid(row=5, column=2)
-        self.ntslim = Entry(frame1)
+        lbl7 = Label(frame2, text="Time (ps)", width=12)
+        lbl7.grid(row=4, column=4)
+        self.ntslim = Entry(frame2,width=5)
         self.ntslim.insert(END, '100')
-        self.ntslim.grid(row=5, column=3)
+        self.ntslim.grid(row=4, column=5)
         print "number of dynamics steps passed to routine? "+self.ntslim.get()
         self.nstlim = int(self.ntslim.get())  # Convert to time steps variable
 
 
         # This is where frame two starts
-        frame2 = Frame(self.parent)
-        frame2.grid(row=2, column=0, sticky="nsew")
+        frame3 = Frame(self.parent)
+        frame3.grid(row=2, column=0, sticky="nsew", columnspan=6, rowspan=5)
         # This next section constructs a box plus the scroll bar and reaction to user input.
-        self.vsb = Scrollbar(frame2, orient="vertical", command=self.OnVsb)
-        self.vsb.grid(row=0, column=2, sticky="ns")
-        lbl7 = Label(frame2, text="List of objects", width=12)
-        lbl7.grid(row=0, column=0)
-        self.lb1 = Listbox(frame2, yscrollcommand=self.vsb.set)
-        self.lb1.grid(row=0, column=1)
+        self.vsb = Scrollbar(frame3, orient="vertical", command=self.OnVsb)
+        self.vsb.grid(row=0, column=2, sticky="wns", rowspan=5)
+        lbl7 = Label(frame3, text="List of objects", width=12)
+        lbl7.grid(row=0, column=0, sticky=N)
+        self.lb1 = Listbox(frame3, yscrollcommand=self.vsb.set)
+        self.lb1.grid(row=0, column=1, rowspan=5)
         self.lb1.bind("<MouseWheel>", self.OnMouseWheel)
         self.lb1.bind("<<ListboxSelect>>", self.OnSelect)
-        objects=cmd.get_names("all")
+
+        objects = cmd.get_names("all")
         objects.extend(["Pick object"])
         for x in objects:
             self.lb1.insert(END, x)
 
 
 
+        # Advanced options frame
+        #frame4 = Frame(self.parent)
+        #frame4.grid(row=2, column=1, sticky="W")
+        lbl8 = Label(frame3, fg="red", text="Advanced options", width=15)
+        lbl8.grid(row=1, column=4, sticky=N)
+
+        # Non standard residue name box
+
+        lbl9 = Label(frame3, fg="red", text="Residue", width=11)
+        lbl9.grid(row=2, column=3, sticky=N)
+        self.residue = Entry(frame3,width=15)
+        self.residue.insert(END, '')
+        self.residue.grid(row=2, column=4, sticky=N+W)
+
         #link = Label(frame2, text="Enlighten home", fg="blue", cursor="hand2")
         #link.bind("<Button-1>", callback)
 
         # self.vsb.config(state=DISABLED)
-        frame3 = Frame(self.parent)
-        frame3.grid(row=5, column=0,columnspan=3, sticky="nsew")
+        frame5 = Frame(self.parent)
+        frame5.grid(row=8, column=0, columnspan=3, sticky="nsew")
         # Three lower buttons in the plugin
-        self.prepButton = Button(frame3, text="RUN PREP", command=self.runprep)
+        self.prepButton = Button(frame5, text="RUN PREP", command=self.runprep)
         self.prepButton.grid(row=0, column=0, sticky="e")
-        self.structButton = Button(frame3, text="RUN STRUCT", command=self.runstruct)
+        self.structButton = Button(frame5, text="RUN STRUCT", command=self.runstruct)
         self.structButton.grid(row=0, column=1, sticky="e")
         self.structButton.config(state=DISABLED)
-        self.dynamButton = Button(frame3, text="RUN DYNAM", command=self.rundynam)
+        self.dynamButton = Button(frame5, text="RUN DYNAM", command=self.rundynam)
         self.dynamButton.grid(row=0, column=2, sticky="e")
         self.dynamButton.config(state=DISABLED)
 
-        self.website = Button(frame3, text="Enlighten Website",fg="blue", command=self.callback)
+        self.website = Button(frame5, text="Enlighten Website",fg="blue", command=self.callback)
         self.website.grid(row=0, column=4, padx=0,sticky="e")
     # This next section defines a series of dialogues that are opened according the the actions from above
     def onOpenF(self):
